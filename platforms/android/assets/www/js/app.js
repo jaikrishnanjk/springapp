eda4cce16c34928159a626733bfb5a445546a0f7
jaikrishnan <jaikrishnan26@gmail.com>
2015-12-18 22:36:44 +0530

    updated code -jai

---
+++ platforms/android/assets/www/js/app.js
@@ -0,0 +1,206 @@
+angular.module('ionicApp', ['ionic'])
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('menu', {
+      url: "/menu",
+      abstract: true,
+      templateUrl: "menu.html",
+      controller: 'MenuCtrl'
+    })
+    .state('menu.tabs', {
+      url: "/tab",
+      views: {
+        'menuContent' :{
+          templateUrl: "tabs.html"
+        }
+      }
+    })
+    .state('menu.tabs.buttons', {
+      url: "/buttons",
+      views: {
+        'buttons-tab': {
+          templateUrl: "buttons.html",
+          controller: 'ButtonsTabCtrl'
+        }
+      }
+    })
+    .state('menu.tabs.list', {
+      url: "/list",
+      views: {
+        'list-tab': {
+          templateUrl: "list.html",
+          controller: 'ListCtrl'
+        }
+      }
+    })
+    .state('menu.tabs.item', {
+      url: "/item",
+      views: {
+        'list-tab': {
+          templateUrl: "item.html"
+        }
+      }
+    }) 
+	.state('menu.tabs.question', {
+      url: "/ques",
+      views: {
+        'list-tab': {
+          templateUrl: "question.html",
+          controller: 'ListCtrl'
+        }
+      }
+    })
+    .state('menu.tabs.form', {
+      url: "/form",
+      views: {
+        'form-tab': {
+          templateUrl: "form.html",
+		  controller: 'ListCtrl'
+        }
+      }
+    })
+    .state('menu.keyboard', {
+      url: "/keyboard",
+      views: {
+        'menuContent': {
+          templateUrl: "keyboard.html"
+        }
+      }
+    })
+    .state('menu.slidebox', {
+      url: "/slidebox",
+      views: {
+        'menuContent': {
+          templateUrl: "slidebox.html",
+          controller: 'SlideboxCtrl'
+        }
+      }
+    })
+    .state('menu.about', {
+      url: "/about",
+      views: {
+        'menuContent': {
+          templateUrl: "about.html"
+        }
+      }
+    });
+
+  $urlRouterProvider.otherwise("menu/tab/buttons");
+
+})
+
+.controller('ListCtrl', function ($scope, $ionicPopup,$timeout,$window) {
+$scope.toggleGroup = function(item) {
+    if ($scope.isGroupShown(item)) {
+      $scope.shownGroup = null;
+    } else {
+      $scope.shownGroup = item;
+    }
+  };
+  $scope.isGroupShown = function(item) {
+    return $scope.shownGroup === item;
+  };
+  $scope.data = {
+    showDelete: false
+  };
+
+  $scope.itemButtons = [
+    {
+      text: 'Delete',
+      type: 'button-assertive',
+      onTap: function (item) {
+        alert('Delete Item: ' + item.id + ' ?');
+      }
+    }
+  ];
+
+  $scope.onItemDelete = function (item) {
+    $scope.items.splice($scope.items.indexOf(item), 1);
+  };
+
+  $scope.items = [
+    {
+      id: "Which Is your favourite colour ?",
+	  options:[
+	  {
+	  opt:"Red"
+	  },
+	  {
+	  opt:"Blue"
+	  }]
+    }
+  ];
+ $scope.showsuccess = function () {
+     
+	   var setChooseTemplate = function() {
+                    $ionicPopup.alert({
+       title: 'Success',
+       content: 'Your Ansewer Submitted successfully'
+     });
+	 $window.location.href="#/menu/tab/list";
+                };
+                $timeout(setChooseTemplate, 1000);
+				
+    };
+})
+
+.controller('ButtonsTabCtrl', function ($scope, $ionicPopup, $ionicActionSheet, $ionicModal) {
+    $scope.showPopup = function () {
+     $ionicPopup.alert({
+       title: 'Popup',
+       content: 'This is ionic popup alert!'
+     });
+    };
+    $scope.showActionsheet = function () {
+        $ionicActionSheet.show({
+          titleText: 'Ionic ActionSheet',
+          buttons: [
+            {
+              text: 'Facebook'
+            },
+            {
+              text: 'Twitter'
+            },
+          ],
+          destructiveText: 'Delete',
+          cancelText: 'Cancel',
+          cancel: function () {
+            console.log('CANCELLED');
+          },
+          buttonClicked: function (index) {
+            console.log('BUTTON CLICKED', index);
+            return true;
+          },
+          destructiveButtonClicked: function () {
+            console.log('DESTRUCT');
+            return true;
+          }
+        });
+    };
+})
+
+.controller('SlideboxCtrl', function($scope, $ionicSlideBoxDelegate) {
+  $scope.nextSlide = function() {
+    $ionicSlideBoxDelegate.next();
+  }             
+})              
+
+.controller('MenuCtrl', function($scope, $ionicSideMenuDelegate, $ionicModal) {              
+  $ionicModal.fromTemplateUrl('modal.html', function (modal) {
+    $scope.modal = modal;
+  }, {
+    animation: 'slide-in-up'
+  });
+ })
+  
+ .controller('AppCtrl', function() {
+
+  ionic.Platform.ready(function() {
+
+  });
+
+ });
+              
+              
